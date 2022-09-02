a3e502aaa599c627ff9f0213d4732c7d8b6ec179
DmitriyZosimov <Dmitriy_Zosimov@epam.com>
2022-09-02 13:12:37 +0300

    Tasks 2-4

--- src/app/reactive-forms/signup-reactive-form/signup-reactive-form.component.ts
+++ src/app/reactive-forms/signup-reactive-form/signup-reactive-form.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, AbstractControl } from '@angular/forms';
+import { UserModel } from './../../models/user.model';
+
 
 @Component({
   selector: 'app-signup-reactive-form',
@@ -7,9 +10,67 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupReactiveFormComponent implements OnInit {
 
+  countries: Array<string> = ['Ukraine', 'Armenia', 'Belarus', 'Hungary', 'Kazakhstan', 'Poland', 'Russia'];
+// data model
+  user: UserModel = new UserModel(
+    'Vitaliy',
+    'Zhyrytskyy',
+    'v.zhiritskiy@gmail.com',
+    false
+  );
+
+  // form model
+  userForm = new FormGroup({
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+    email: new FormControl(''),
+    sendProducts: new FormControl(true)
+  });
+
   constructor() { }
 
   ngOnInit() {
+    this.setFormValues();
+    // this.patchFormValues();
+
   }
+
+  onSave(): void {
+    // Form model
+    console.log(this.userForm);
+    // Form value w/o disabled controls
+    console.log(`Saved: ${JSON.stringify(this.userForm.value)}`);
+    // Form value w/ disabled controls
+    console.log(`Saved: ${JSON.stringify(this.userForm.getRawValue())}`);
+  }
+
+  get firstName(): AbstractControl {
+    return this.userForm.get('firstName')!;
+  }
+
+  get lastName(): AbstractControl {
+    return this.userForm.get('lastName')!;
+  }
+
+  get email(): AbstractControl {
+    return this.userForm.get('email')!;
+  }
+
+  private setFormValues(): void {
+    this.userForm.setValue({
+      firstName: this.user.firstName,
+      lastName: this.user.lastName,
+      email: this.user.email,
+      sendProducts: this.user.sendProducts
+    });
+  }
+
+  private patchFormValues(): void {
+    this.userForm.patchValue({
+      firstName: this.user.firstName,
+      lastName: this.user.lastName
+    });
+  }
+
 
 }
