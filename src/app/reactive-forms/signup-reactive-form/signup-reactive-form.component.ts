68fd6922ce8042bcd2acc4d8135df9e14988b054
DmitriyZosimov <Dmitriy_Zosimov@epam.com>
2022-09-05 11:42:33 +0300

    Task 7. Adjust validators

--- src/app/reactive-forms/signup-reactive-form/signup-reactive-form.component.ts
+++ src/app/reactive-forms/signup-reactive-form/signup-reactive-form.component.ts
@@ -19,6 +19,11 @@ export class SignupReactiveFormComponent implements OnInit {
     false
   );
 
+  placeholder = {
+    email: 'Email (required)',
+    phone: 'Phone'
+  };
+
   // form model
   // userForm = new FormGroup({
   //   firstName: new FormControl(''),
@@ -41,7 +46,8 @@ export class SignupReactiveFormComponent implements OnInit {
         Validators.email
       ]
     ],
-
+    phone: '',
+    notification: 'email',
     sendProducts: true
   });
 
@@ -79,14 +85,43 @@ export class SignupReactiveFormComponent implements OnInit {
     return this.userForm.get('email')!;
   }
 
-  private setFormValues(): void {
-    this.userForm.setValue({
-      firstName: this.user.firstName,
-      lastName: { value: this.user.lastName, disabled: false },
-      email: this.user.email,
-      sendProducts: this.user.sendProducts
-    });
+  get phone(): AbstractControl {
+    return this.userForm.get('phone')!;
   }
+
+  // private setFormValues(): void {
+  //   this.userForm.setValue({
+  //     firstName: this.user.firstName,
+  //     lastName: { value: this.user.lastName, disabled: false },
+  //     email: this.user.email,
+  //     sendProducts: this.user.sendProducts
+  //   });
+  // }
+
+  onSetNotification(notifyVia: string): void {
+    const phoneControl = this.phone;
+    const emailControl = this.email;
+
+    if (notifyVia === 'text') {
+      phoneControl.setValidators(Validators.required);
+      emailControl.clearValidators();
+      this.placeholder.email = 'Email';
+      this.placeholder.phone = 'Phone (required)';
+    }
+    else {
+      emailControl.setValidators( [
+        Validators.required,
+        Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+'),
+        Validators.email
+      ]);
+      phoneControl.clearValidators();
+      this.placeholder.email = 'Email (required)';
+      this.placeholder.phone = 'Phone';
+    }
+    phoneControl.updateValueAndValidity();
+    emailControl.updateValueAndValidity();
+  }
+
 
   private patchFormValues(): void {
     this.userForm.patchValue({
